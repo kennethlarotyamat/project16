4d3b80d187373387b516299414a579a9749038db
Kenneth Larot Yamat <kenneth.larot.yamat@gmail.com>
2024-02-21 06:17:52 -0800

    1

--- javascript/project16.js
+++ javascript/project16.js
@@ -24,5 +24,22 @@ Everything up-to-date
 
 
 ---
+Nothing
+seems to be working
+and this is a headache
+
+I can't seem to get anything done.
+
+This isn't really important is it.
+
+I don't know if I just need to leave it where it is.
+
+I'm getting worried. 
+
+> git push origin main:main
+error: RPC failed; HTTP 400 curl 92 HTTP/2 stream 7 was not closed cleanly: CANCEL (err 8)
+send-pack: unexpected disconnect while reading sideband packet
+fatal: the remote end hung up unexpectedly
+Everything up-to-date
 
 */
