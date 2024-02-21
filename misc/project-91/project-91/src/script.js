1644885dffb11c1ea2cf13f869eadfa9cfd7d8dc
Kenneth Larot Yamat <kenneth.larot.yamat@gmail.com>
2024-02-21 04:34:27 -0800

    1

--- misc/project-91/project-91/src/script.js
+++ misc/project-91/project-91/src/script.js
@@ -29,6 +29,12 @@ send-pack: unexpected disconnect while reading sideband packet
 fatal: the remote end hung up unexpectedly
 Everything up-to-date
 
+> git push origin main:main
+error: RPC failed; HTTP 400 curl 92 HTTP/2 stream 7 was not closed cleanly: CANCEL (err 8)
+send-pack: unexpected disconnect while reading sideband packet
+fatal: the remote end hung up unexpectedly
+Everything up-to-date
+
 
 
 */
